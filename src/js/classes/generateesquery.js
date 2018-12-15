5d3a5b11d6bc13a76adf3f5d488eee8931f7caf2
ourhiddenhistory <info@ourhiddenhistory>
2018-12-14 21:23:11 -0500

    fix page query

--- src/js/classes/generateesquery.js
+++ src/js/classes/generateesquery.js
@@ -9,21 +9,22 @@ class GenerateEsQuery {
     const esQuery = {
       query: {
         bool: {
-          must: [],
-        },
-      },
+          must: []
+        }
+      }
     };
     const stringParts = stringInput.match(/("[^"]+"|[^"\s]+)/g);
-    stringParts.forEach((str) => {
+
+    stringParts.forEach(str => {
       const searchObj = {};
-      const cleanStr = str.replace(/^"([^"]+)"$/, '$1').replace(/\s+/, ' ');
+      const cleanStr = str.replace(/^"([^"]+)"$/, "$1").replace(/\s+/, " ");
       if (GenerateEsQuery.isPhrase(cleanStr)) {
         searchObj.match_phrase = {
-          content: cleanStr,
+          content: cleanStr
         };
       } else {
         searchObj.match = {
-          content: cleanStr,
+          content: cleanStr
         };
       }
       esQuery.query.bool.must.push(searchObj);
@@ -36,6 +37,24 @@ class GenerateEsQuery {
    * @returns {Boolean} is phrase?
    */
   static isPhrase(str) {
-    return (str.indexOf(' ') >= 0);
+    return str.indexOf(" ") >= 0;
+  }
+  /**
+   * Create a point for a page.
+   * @param {String} stringInput - user search string.
+   * @returns {Object} json representation of search
+   */
+  static generatePage(stringInput) {
+    const fieldQuery = {
+      query: {
+        constant_score: {
+          filter: {
+            term: {}
+          }
+        }
+      }
+    };
+    fieldQuery.query.constant_score.filter.term['file.filename'] = stringInput
+    return fieldQuery;
   }
 }
