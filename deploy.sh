1441842fc3712328f2bccf4d1622ea8778278b10
dmgig <dave.m.giglio@gmail.com>
2019-03-20 23:24:50 -0400

    add circleci, add config

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/#!/usr/bin/env bash
 
-gulp build
-jekyll build --verbose --trace
-rsync -pruv -e 'ssh -p 2222' \
- --delete-after  \
- --exclude utils \
- --exclude docsData \
- ~/Repos/doc-search-blog/_site/ useful@50.87.146.99:/home2/useful/ourhiddenhistory.com/public/doc-search/
+# gulp build
+# jekyll build --verbose --trace
+rsync -acr --stats -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
+   --exclude .git \
+   --delete-after \
+   --exclude utils \
+   --exclude docsData \
+   . useful@50.87.146.99:/home2/useful/b.ourhiddenhistory.com/public/doc-search/
