6c91490f2ea333c79eacf897d77227d254f340cc
dmgig <dave.m.giglio@gmail.com>
2019-03-21 13:14:46 -0400

    create parent directories

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 DOMAIN="b-${GIT_BRANCH}.ourhiddenhistory.org"
 
+mkdir --parents /home2/useful/${DOMAIN}/html/doc-search
+
 rsync -acr --stats -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
    ./_site/ useful@50.87.146.99:/home2/useful/${DOMAIN}/html/doc-search
