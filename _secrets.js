268e1b8e9ee49153186285cc4f9c0834794c940d
dmgig <dave.m.giglio@gmail.com>
2019-03-22 01:08:38 -0400

    Delete _secrets.js

--- _secrets.js
+++
@@ -1,7 +0,0 @@
-const yimp = require('yaml-import');
-const path = require('path');
-
-const filepath = path.join(__dirname, '_secrets.yml');
-const secrets = yimp.read(filepath);
-
-module.exports = secrets;
